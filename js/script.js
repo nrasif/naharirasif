ae7a5e16e931c1643903769280870a8a7088ec54
Nahari Rasif <naharirasif18@gmail.com>
2023-02-23 00:51:56 +0700

    Initialize Portfolio Website

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["physicist","science Data Scientist","science Data Analyst"],
+    typeSpeed: 70,
+    backSpeed: 100,
+    backDelay: 100,
+    loop: true
+})
