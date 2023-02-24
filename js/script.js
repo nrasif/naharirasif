815f3a2a111880a90400f8202cf2198f7d7ec8d6
Nahari Rasif <naharirasif18@gmail.com>
2023-02-24 11:09:30 +0700

    editing header and adding navbar

--- js/script.js
+++ js/script.js
@@ -5,3 +5,18 @@ var typed = new Typed(".multiple-text", {
     backDelay: 100,
     loop: true
 })
+
+// Get the container element
+var btnContainer = document.getElementById("navbar");
+
+// Get all buttons with class="btn" inside the container
+var btns = btnContainer.getElementsByClassName("nav-btn");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
